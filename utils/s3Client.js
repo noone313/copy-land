e76fef69ed049bda78d6678131fecd1aab10b6b5
baqer ali <124620943+noone313@users.noreply.github.com>
2025-05-09 08:34:52 +0300

    Add files via upload

--- utils/s3Client.js
+++ utils/s3Client.js
@@ -6,7 +6,7 @@ const s3Client = new S3Client({
   region: process.env.AWS_REGION,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID_,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_,
   },
 });
 
